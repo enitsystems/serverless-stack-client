af6d6e218b5854f4beba61f7c7bdc6b5c27a5a93
Torsten Loos <2260337+torst3n@users.noreply.github.com>
2020-04-07 14:52:29 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-central-1",
+      BUCKET: "notes-app-uploads-enit"
+    },
+    apiGateway: {
+      REGION: "eu-central-1",
+      URL: "https://qws9n3n4jb.execute-api.eu-central-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-central-1",
+      USER_POOL_ID: "eu-central-1_eKuD5SMCh",
+      APP_CLIENT_ID: "36sqddag0hpts9ekf54f5qnutq",
+      IDENTITY_POOL_ID: "eu-central-1:de72fcc9-8f7a-47af-9c63-6afd4be54c76"
+    }
+  };
